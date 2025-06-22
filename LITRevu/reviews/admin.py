21173991e2c5c9536c6ef3a4dd0a0dafe17d42e4
xjeanne <x.jeanne@102-103.com>
2025-06-22 22:55:01 +0200

    Step 3 : Add model for ticket  / review / follows in admin systeme

--- LITRevu/reviews/admin.py
+++ LITRevu/reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Ticket, Review, UserFollows
 
-# Register your models here.
+
+class TicketAdmin(admin.ModelAdmin):
+    pass
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserFollowsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollows, UserFollowsAdmin)
