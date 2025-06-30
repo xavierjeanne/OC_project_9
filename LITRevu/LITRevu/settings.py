2f75b35c17ea0b8402643b713204253d5231abfe
xjeanne <x.jeanne@102-103.com>
2025-06-30 15:18:34 +0200

    Step 7 :  design template login / sign up

--- LITRevu/LITRevu/settings.py
+++ LITRevu/LITRevu/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'authentication',
     'tailwind',
     'theme',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- LITRevu/theme/static/js/app.js
+++ LITRevu/theme/static/js/app.js
@@ -3,25 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const btn = document.getElementById('menu-toggle');
     const mobileMenu = document.getElementById('mobile-menu');
   
-    btn.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-      
-    });
-  
-    document.addEventListener('click', (e) => {
-      if (!btn.contains(e.target) && !mobileMenu.contains(e.target)) {
-        mobileMenu.classList.add('hidden');
-      }
-    });
+    if (btn) {
+        btn.addEventListener('click', () => {
+            if (mobileMenu) {
+                mobileMenu.classList.toggle('hidden');
+                document.addEventListener('click', (e) => {
+                  if (!btn.contains(e.target) && !mobileMenu.contains(e.target)) {
+                    mobileMenu.classList.add('hidden');
+                  }
+                });
+            }
+        });
+    }
     window.closeMenu = function () {
         mobileMenu.classList.add('hidden');
       };
   
-      
-    document.addEventListener('click', (e) => {
-        if (!btn.contains(e.target) && !mobileMenu.contains(e.target)) {
-          mobileMenu.classList.add('hidden');
-        }
-    });
   });
   
